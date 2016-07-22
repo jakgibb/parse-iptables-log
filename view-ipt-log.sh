7269518b083975ad866afdebe49184b39a25d783
Jak Gibb <jakgibb@users.noreply.github.com>
2016-07-22 16:42:31 +0100

    Update view-ipt-log.sh

--- view-ipt-log.sh
+++ view-ipt-log.sh
@@ -1,10 +1,10 @@
 echo "----------------------------------------------------------"
 echo "			Blocked Log				"
 echo "----------------------------------------------------------"
-cat /var/log/iptables/iptables.blocked.log | /usr/bin/python2.7 /usr/local/bin/parse-iptlog.py
+/usr/bin/python2.7 /usr/local/bin/parse-iptlog.py < /var/log/iptables/iptables.blocked.log
 echo "Press enter to read allowed log"
 read
 echo "----------------------------------------------------------"
 echo "			Allowed Log				"
 echo "----------------------------------------------------------"
-cat /var/log/iptables/iptables.allowed.log |  /usr/bin/python2.7 /usr/local/bin/parse-iptlog.py
+/usr/bin/python2.7 /usr/local/bin/parse-iptlog.py < /var/log/iptables/iptables.allowed.log
